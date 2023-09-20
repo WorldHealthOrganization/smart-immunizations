1ce96c46e5f2a9a2ceed2ecffe49c717edcbd8ce
Luke Duncan <lduncan@path.org>
2023-09-20 06:14:18 -0700

    Added Library files for updated CQL.

--- _refresh.sh
+++ _refresh.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-1.4.1-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=tooling-cli-2.4.0.jar
 input_cache_path=./input-cache
-resources_path=$PWD/input/resources
-ig_ini_path=./ig.ini
+ig_ini_path=$PWD/ig.ini
 
 set -e
 echo Checking internet connection...
@@ -12,7 +11,6 @@ wget -q --spider tx.fhir.org
 if [ $? -eq 0 ]; then
 	echo "Online"
 	fsoption=""
-#"-fs http://cds-sandbox.alphora.com/cqf-ruler-r4/fhir/"
 else
 	echo "Offline"
 	fsoption=""
@@ -22,12 +20,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	java -Xmx2048M -jar $tooling -RefreshIG -ini="$ig_ini_path" -t -d -p $fsoption
+	java -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		java -Xmx2048M -jar $tooling -RefreshIG -ini="$ig_ini_path" -t -d -p $fsoption
+		java -jar $tooling -RefreshIG -ini="$ig_ini_path" -d -p -t $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
